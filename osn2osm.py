7540e4944443fe3a77518a5ee719a3e31e3fb66b
Pavel Tyslacki <pavel.tyslyatsky@gmail.com>
2015-05-21 23:38:31 +0300

    init

---
+++ osn2osm.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+import sys
+
+import jinja2
+import lxml.etree
+
+
+template = '''<?xml version="1.0" encoding="UTF-8"?>
+<osm version="0.6" generator="osm notes">
+  <bounds minlon="-180" minlat="-90" maxlon="180" maxlat="90" origin="http://www.openstreetmap.org/api/0.6"/>
+{% for note in notes %}
+  <node id="{{ note.id }}" version="{{ note.version }}" timestamp="{{ note.timestamp }}" uid="{{ note.uid }}" user="{{ note.user }}" changeset="{{ note.changeset }}" lat="{{ note.lat }}" lon="{{ note.lon }}">
+    <tag k="title" v="{{ note.title }}"/>
+    <tag k="content" v="{{ note.content }}"/>
+  </node>{% endfor %}
+</osm>
+'''
+
+
+class Comment(object):
+
+    __slots__ = ('action', 'timestamp', 'uid', 'user', 'text')
+
+    def __init__(self, element):
+        self.action = element.attrib['action']
+        self.timestamp = element.attrib['timestamp']
+        self.uid = element.attrib.get('uid', 0)
+        self.user = element.attrib.get('user', 'anonimous')
+        self.text = element.text
+
+
+class Note(object):
+
+    __slots__ = ('id', 'lat', 'lon', 'created_at', 'closed_at', 'comments')
+
+    def __init__(self, element):
+        self.id = element.attrib['id']
+        self.lat = element.attrib['lat']
+        self.lon = element.attrib['lon']
+        self.created_at = element.attrib['created_at']
+        self.closed_at = element.attrib.get('closed_at')
+        self.comments = tuple(Comment(_) for _ in element.iterchildren('comment'))
+
+    @property
+    def version(self):
+        return len(self.comments)
+
+    @property
+    def changeset(self):
+        return self.id
+
+    @property
+    def user(self):
+        return self.comments[0].user
+
+    @property
+    def uid(self):
+        return self.comments[0].uid
+
+    @property
+    def timestamp(self):
+        return self.created_at
+
+    @property
+    def title(self):
+        title = self.comments[0].text
+        if len(title) > 77:
+            return '%s...' % title[:77]
+        return title
+
+    @property
+    def content(self):
+        return '\n'.join(
+            '%s by %s at %s: %s' % (_.action, _.user, _.timestamp, _.text)
+            for _ in self.comments
+        )
+
+
+def _clear_xml_element(element):
+    element.clear()
+    for ancestor in element.xpath('ancestor-or-self::*'):
+        while ancestor.getprevious() is not None:
+            del ancestor.getparent()[0]
+
+
+def parse_notes(notes_dump):
+    for event, element in lxml.etree.iterparse(notes_dump, events=('end',),
+                                               tag='note', recover=True):
+        if not element.attrib.get('closed_at'):
+            yield Note(element)
+        _clear_xml_element(element)
+
+
+def do():
+    stdin = sys.stdin if sys.version_info.major == 2 else sys.stdin.buffer
+    stdout = sys.stdout if sys.version_info.major == 2 else sys.stdout.buffer
+    notes = parse_notes(stdin)
+    for chunk in jinja2.Template(template, autoescape=True).generate(notes=notes):
+        stdout.write(chunk.encode('utf8'))
+
+
+if __name__ == '__main__':
+    do()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='osn2osm',
+    version='0.0.1',
+    description='Transform OpenStreetMap notes (*.osn) to (*.osm) file.',
+    author='Pavel Tyslacki',
+    author_email='pavel.tyslacki@gmail.com',
+    license='The MIT License: http://opensource.org/licenses/MIT',
+    packages=find_packages(),
+    scripts=['osn2osm.py'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=open('requirements.txt').read().strip().splitlines(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+    ],
+)
